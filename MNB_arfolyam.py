e71b0021e960d82012869b80ebf40c03ad8820d9
Botond Barabas <botond.barabas@ni.com>
2018-06-24 13:54:19 +0200

    Raising exception in case of a non-workday in Hungary was queried

--- MNB_arfolyam.py
+++ MNB_arfolyam.py
@@ -14,10 +14,6 @@ def MNBArfolyam(date, currency="EUR"):
     Simple tool for querying the currency exchange rate of the
     Hungarian National Bank for one particular day
 
-    TODO:
-    1) if query response is 1, that means that the date was not a working day
-        this has to be checked by the function
-
     Input parameters:
         date: the date you are looking for, has to be yyyy.mm.dd. format!
         currency: a three letter string, eg: "EUR", defaults to "EUR" as Euros
@@ -43,11 +39,17 @@ def MNBArfolyam(date, currency="EUR"):
     html = response.text
     soup = BeautifulSoup(html, "html.parser")
     # print(soup)
-    return float(soup.select("div > span")[-1].contents[0].replace(',', '.'))
+    exchangeRate = float(soup.select("div > span")[-1].contents[0].
+                         replace(',', '.'))
+    if exchangeRate == 1.0:
+        raise ValueError("The date requested:"
+                         " {} was a holiday in Hungary".format(date))
+    else:
+        return exchangeRate
 
 
 dates = ["2018.01.22.", "2018.02.20.", "2018.03.20.", "2018.04.20.",
-         "2018.05.22.", "2018.06.20."]
+         "2018.05.20.", "2018.06.20."]
 
 for date in dates:
     print(date + ": ", MNBArfolyam(date))
