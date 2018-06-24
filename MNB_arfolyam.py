65f27b88bc74b0319da48da672849dbb2ac5e14c
Botond Barabas <botond.barabas@ni.com>
2018-06-24 12:27:00 +0200

    support for multiple currencies implemented

--- MNB_arfolyam.py
+++ MNB_arfolyam.py
@@ -3,24 +3,24 @@ MNB árfolyam puller.
 
 Simple tool for querying the currency exchange rate of the
 Hungarian National Bank for one particular day
-Currently only EUR is supported
 """
 from time import sleep
 
 
-def MNBArfolyam(date):
+def MNBArfolyam(date, currency="EUR"):
     """
     MNB árfolyam puller.
 
     Simple tool for querying the currency exchange rate of the
     Hungarian National Bank for one particular day
-    Currently only EUR is supported
+
     TODO:
     1) if query response is 1, that means that the date was not a working day
         this has to be checked by the function
-    2) implement other currencies
+
     Input parameters:
         date: the date you are looking for, has to be yyyy.mm.dd. format!
+        currency: a three letter string, eg: "EUR", defaults to "EUR" as Euros
     """
     import requests
     from bs4 import BeautifulSoup
@@ -31,16 +31,15 @@ def MNBArfolyam(date):
     except ValueError:
         raise ValueError("Incorrect date format, should be YYYY.mm.dd.")
 
-    # potentially implement ither currencies
-    url_head = "https://www.mnb.hu/arfolyam-tablazat?"
-    url = "deviza=rbCurrencySelect&devizaSelected=EUR&datefrom={}&datetill={}"
-    url_tail = "&order=1"
-    url = url_head+url+url_tail
+    url = "https://www.mnb.hu/arfolyam-tablazat?" \
+          "deviza=rbCurrencySelect" \
+          "&devizaSelected={}" \
+          "&datefrom={}" \
+          "&datetill={}" \
+          "&order=1"
     # print(url)
 
-    # validate date
-
-    response = requests.get(url.format(date, date))
+    response = requests.get(url.format(currency, date, date))
     html = response.text
     soup = BeautifulSoup(html, "html.parser")
     # print(soup)
@@ -52,4 +51,8 @@ dates = ["2018.01.22.", "2018.02.20.", "2018.03.20.", "2018.04.20.",
 
 for date in dates:
     print(date + ": ", MNBArfolyam(date))
+    sleep(1)
+
+for date in dates:
+    print(date + ": ", MNBArfolyam(date, "USD"))
     sleep(1)
